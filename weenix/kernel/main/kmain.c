2d5a2050f5e7050624a6754de13527f931a784f6
say543 <say543@say543-VirtualBox.(none)>
2013-11-28 01:11:09 -0800

    leave the comment of this commitment

--- weenix/kernel/main/kmain.c
+++ weenix/kernel/main/kmain.c
@@ -355,7 +355,7 @@ initproc_run(int arg1, void *arg2)
 		kernel_execve("/usr/bin/args",argv,envp);
 		kernel_execve("/bin/uname",argv,envp);
 */
-		kernel_execve("/usr/bin/stress",argv,envp);
+		kernel_execve("/usr/bin/fork-and-wait",argv,envp);
 /*
         kshell_add_command("testproc",(kshell_cmd_func_t)testproc,"Ted Faber's tests");
         kshell_add_command("shtest",(kshell_cmd_func_t)sunghan_test,"sunghan's tests");
